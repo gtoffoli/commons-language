00b9e3964e9afcc18dc5f0788f529757c990122d
Giovanni Toffoli <toffoli@linkroma.it>
2022-05-08 11:26:57 +0200

    Extended scripts for working at the Croatian model.

--- nlp/spacy_custom/hr/scripts.py
+++ nlp/spacy_custom/hr/scripts.py
@@ -33,8 +33,22 @@ def settimes_to_iob(in_path):
         evalfile.close()
     infile.close()
 
+CONLLU_DICT = {
+    'ID': 0, # Word index
+    'FORM': 1, # Word form or punctuation symbol.
+    'LEMMA': 2, # Lemma or stem of word form.
+    'UPOS': 3, # Universal part-of-speech tag.
+    'XPOS': 4, # Language-specific part-of-speech tag; underscore if not available.
+    'FEATS': 5, # List of morphological features from the universal feature inventory or from a defined language-specific extension; underscore if not available.
+    'HEAD': 6, # Head of the current word, which is either a value of ID or zero (0).
+    'DEPREL': 7, # Universal dependency relation to the HEAD (root iff HEAD = 0) or a defined language-specific subtype of one.
+    'DEPS': 8, # Enhanced dependency graph in the form of a list of head-deprel pairs.
+    'MISC': 9, # Any other annotation.
+    'IOB': 9, # annotation for NER.
+}
+
 def normalize_split_hr500k(in_path):
-    """ From the conll format of the hr500k treebank,
+    """ From the conll format of the hr500k.conllu treebank,
         remove the comment lines,
         extract only the first 11 columns (10 + iob) to be used as training data
         and keep only sentences just after comment # sent_id = train.. or # sent_id = test.. 
@@ -44,36 +58,45 @@ def normalize_split_hr500k(in_path):
     file_name = in_path.replace(file_ext, '')
     train_path = file_name+'-train'+file_ext
     test_path = file_name+'-test'+file_ext
-    # choices = []
-    outfile = None
+    choices = []
     empty = True
+    skip = False
     with open(in_path, encoding='utf8') as infile:
         with open(train_path, 'w', encoding='utf8') as trainfile:
+            outfile = trainfile
             with open(test_path, 'w', encoding='utf8') as testfile:
-                """
                 if not choices:
-                    choices = [trainfile, trainfile, trainfile, evalfile]
-                """
+                    choices = [trainfile, trainfile, trainfile, testfile]
                 line = infile.readline()
                 while(line):
-                    if empty:
-                        if line.startswith('#'):
-                            if line.startswith('# sent_id = train'):
-                                outfile = trainfile
-                            elif line.startswith('# sent_id = test'):
-                                outfile = testfile
-                            line = infile.readline()
-                            continue
-                    fields = line.replace('\n','').split('\t')
-                    if len(fields) > 1:
-                        line = '\t'.join(fields[:11])+'\n'
-                        empty = False
+                    if line.startswith('#'):
+                        if line.startswith('# sent_id'):
+                            outfile = choices[randint(0, 3)]
+                        elif not line.startswith('# text'):
+                            pass
+                        line = infile.readline()
+                        continue
                     else:
-                        empty = True
-                    if outfile:
+                        fields = line.replace('\r','').replace('\n','').split('\t')
+                        if len(fields) >= 10:
+                            if fields[CONLLU_DICT['HEAD']]=='_' or fields[CONLLU_DICT['DEPREL']]=='_':
+                                skip = True
+                            else:
+                                misc_list = fields[CONLLU_DICT['MISC']].split('|')
+                                iob = 'O'
+                                for item in misc_list:
+                                    if item.startswith('NER='):
+                                        iob = item.replace('NER=', '').upper()
+                                        break
+                                fields = fields[:CONLLU_DICT['MISC']]+[iob]+fields[CONLLU_DICT['MISC']+1:]
+                                line = '\t'.join(fields)+'\n'
+                            empty = False
+                        else:
+                            empty = True
+                    if empty or not skip:
                         outfile.write(line)
                     if empty:
-                        outfile = None
+                        skip = False
                     line = infile.readline()
         trainfile.close()
         testfile.close()
