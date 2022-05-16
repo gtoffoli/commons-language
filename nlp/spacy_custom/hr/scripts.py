debd4a3f472b036f1ce62840ad3f0a380020a252
Giovanni Toffoli <toffoli@linkroma.it>
2022-05-16 12:05:10 +0200

    Updated script to normalize and split the hr500k repository.

--- nlp/spacy_custom/hr/scripts.py
+++ nlp/spacy_custom/hr/scripts.py
@@ -3,34 +3,37 @@ from random import randint
 def settimes_to_iob(in_path):
     """ From the conllx (extended conllu) format of the SETimes.HR treebank,
         extract only the form and iob fields to be used as NER training data
-        and split them between train and eval files in the 3:1 ratio.
+        and split them between train, eval and test files in the 3:2:1 ratio.
         see: https://universaldependencies.org/docs/format.html 
              http://nlp.ffzg.hr/resources/corpora/setimes-hr/
              https://www.clarin.si/repository/xmlui/handle/11356/1183 """
     train_path = in_path+'-train.iob'
     eval_path = in_path+'-eval.iob'
+    test_path = in_path+'-test.iob'
     choices = []
     empty = True
     with open(in_path, encoding='utf8') as infile:
         with open(train_path, 'w', encoding='utf8') as trainfile:
             with open(eval_path, 'w', encoding='utf8') as evalfile:
-                if not choices:
-                    choices = [trainfile, trainfile, trainfile, evalfile]
-                line = infile.readline()
-                while(line):
-                    if empty:
-                        outfile = choices[randint(0, 3)]
-                    fields = line.replace('\n','').split('\t')
-                    if len(fields) > 1:
-                        id, form, lemma, upos, xpos, iob, head, deprel, deps, misc = fields 
-                        line = '\t'.join([form, iob])+'\n'
-                        empty = False
-                    else:
-                        empty = True
-                    outfile.write(line)
+                with open(test_path, 'w', encoding='utf8') as testfile:
+                    if not choices:
+                        choices = [trainfile, trainfile, trainfile, evalfile, evalfile, testfile]
                     line = infile.readline()
+                    while(line):
+                        if empty:
+                            outfile = choices[randint(0, 5)]
+                        fields = line.replace('\n','').split('\t')
+                        if len(fields) > 1:
+                            id, form, lemma, upos, xpos, iob, head, deprel, deps, misc = fields 
+                            line = '\t'.join([form, iob])+'\n'
+                            empty = False
+                        else:
+                            empty = True
+                        outfile.write(line)
+                        line = infile.readline()
         trainfile.close()
         evalfile.close()
+        testfile.close()
     infile.close()
 
 CONLLU_DICT = {
@@ -50,12 +53,13 @@ CONLLU_DICT = {
 def normalize_split_hr500k(in_path):
     """ From the conll format of the hr500k.conllu treebank,
         remove the comment lines and keep only sentences with UD annotation, 
-        split them between train and test files in the 3:1 ratio,
-        extract from them the first 10 columns
+        split them between train, eval and test files in the 3:2:1 ratio,
+        keep only IOB data from MISC field
         see: https://github.com/reldi-data/hr500k/blob/master/hr500k.conllu """
     file_ext = '.'+in_path.split('.')[-1]
     file_name = in_path.replace(file_ext, '')
     train_path = file_name+'-train'+file_ext
+    eval_path = file_name+'-eval'+file_ext
     test_path = file_name+'-test'+file_ext
     choices = []
     empty = True
@@ -63,40 +67,42 @@ def normalize_split_hr500k(in_path):
     with open(in_path, encoding='utf8') as infile:
         with open(train_path, 'w', encoding='utf8') as trainfile:
             outfile = trainfile
-            with open(test_path, 'w', encoding='utf8') as testfile:
-                if not choices:
-                    choices = [trainfile, trainfile, trainfile, testfile]
-                line = infile.readline()
-                while(line):
-                    if line.startswith('#'):
-                        if line.startswith('# sent_id'):
-                            outfile = choices[randint(0, 3)]
-                        elif not line.startswith('# text'):
-                            pass
-                        line = infile.readline()
-                        continue
-                    else:
-                        fields = line.replace('\r','').replace('\n','').split('\t')
-                        if len(fields) >= 10:
-                            if fields[CONLLU_DICT['HEAD']]=='_' or fields[CONLLU_DICT['DEPREL']]=='_':
-                                skip = True
-                            else:
-                                misc_list = fields[CONLLU_DICT['MISC']].split('|')
-                                iob = 'O'
-                                for item in misc_list:
-                                    if item.startswith('NER='):
-                                        iob = item.replace('NER=', '').upper()
-                                        break
-                                fields = fields[:CONLLU_DICT['MISC']]+[iob]+fields[CONLLU_DICT['MISC']+1:]
-                                line = '\t'.join(fields)+'\n'
-                            empty = False
-                        else:
-                            empty = True
-                    if empty or not skip:
-                        outfile.write(line)
-                    if empty:
-                        skip = False
+            with open(eval_path, 'w', encoding='utf8') as evalfile:
+                with open(test_path, 'w', encoding='utf8') as testfile:
+                    if not choices:
+                        choices = [trainfile, trainfile, trainfile, evalfile, evalfile, testfile]
                     line = infile.readline()
+                    while(line):
+                        if line.startswith('#'):
+                            if line.startswith('# sent_id'):
+                                outfile = choices[randint(0, 5)]
+                            elif not line.startswith('# text'):
+                                pass
+                            line = infile.readline()
+                            continue
+                        else:
+                            fields = line.replace('\r','').replace('\n','').split('\t')
+                            if len(fields) >= 10:
+                                if fields[CONLLU_DICT['HEAD']]=='_' or fields[CONLLU_DICT['DEPREL']]=='_':
+                                    skip = True
+                                else:
+                                    misc_list = fields[CONLLU_DICT['MISC']].split('|')
+                                    iob = 'O'
+                                    for item in misc_list:
+                                        if item.startswith('NER='):
+                                            iob = item.replace('NER=', '').upper()
+                                            break
+                                    fields = fields[:CONLLU_DICT['MISC']]+[iob]+fields[CONLLU_DICT['MISC']+1:]
+                                    line = '\t'.join(fields)+'\n'
+                                empty = False
+                            else:
+                                empty = True
+                        if empty or not skip:
+                            outfile.write(line)
+                        if empty:
+                            skip = False
+                        line = infile.readline()
         trainfile.close()
+        evalfile.close()
         testfile.close()
     infile.close()
