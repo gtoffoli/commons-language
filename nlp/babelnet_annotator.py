e729982389ab53d761b5bb3390c3fd8cc1fa2149
Giovanni Toffoli <toffoli@linkroma.it>
2023-04-20 23:46:58 +0200

    Added domains to the BabelNet annotation.

--- nlp/babelnet_annotator.py
+++ nlp/babelnet_annotator.py
@@ -1,9 +1,11 @@
 # module derived from:
 # https://github.com/asterbini/spacy-babelnet/blob/master/spacy_babelnet/babelnet_annotator.py
 
+from operator import itemgetter
+import json
+
 from spacy.tokens.doc      import Doc
 from spacy.tokens.token    import Token
-import json
 
 import babelnet as bn
 from babelnet.pos import POS
@@ -32,7 +34,10 @@ class BabelnetAnnotator:
         for token in doc:
             babelnet = Babelnet(token=token, lang=self.__bn_lang, domains=self.__bn_domains)
             # token._.set(BabelnetAnnotator.__FIELD, babelnet)
-            annotation = [[s.id.id, s.main_sense().source.source_name, s.main_gloss().gloss] for s in babelnet.synsets()]
+            annotation = [
+                [s.id.id, s.main_gloss().gloss, s.main_sense().source.source_name, \
+                 sorted([[k.domain_string, v] for k,v in s.domains.items()], key=itemgetter(1), reverse=True)] \
+                for s in babelnet.synsets()]
             if settings.DEBUG:
                 print('---', token, annotation)
             token._.set(BabelnetAnnotator.__FIELD, annotation)
@@ -84,6 +89,12 @@ class Babelnet():
     # we cache the synsets for a (word, pos) pair at class level
     cached_synsets = {}
 
+    def domains_ok(self, synset):
+        for k, v in synset.domains.items():
+            if v >= 4.0:
+                return True
+        return False
+
     def __word_synsets(self, pos, words, poses):
         '''retrieve the sysnsets for a given (word, pos)'''
         lemma = words[0]
@@ -97,15 +108,17 @@ class Babelnet():
                 print('- filtered by domain:', [[s.id, s] for s in synsets_2])
             synsets = []
             for s in synsets_2:
-                print('- s: ', key, s.id, s.type, s.main_sense().source.source_name)
+                # print('- s: ', key, s.id, s.type, s.main_sense().source.source_name)
                 s_ok = False
                 lemma_objects = s.lemmas(self.__bn_lang)
                 for lo in lemma_objects:
-                    print('- lo:', lo.lemma, lo.lemma_type)
-                    if lo.lemma.lower() == lemma.lower() and lo.lemma_type == BabelLemmaType.HIGH_QUALITY and \
+                    # print('- lo:', lo.lemma, lo.lemma_type)
+                    #? if lo.lemma.lower() == lemma.lower() and lo.lemma_type == BabelLemmaType.HIGH_QUALITY and \
+                    if lo.lemma_type == BabelLemmaType.HIGH_QUALITY and \
                        ((pos in ['VERB', 'NOUN', 'ADJ',] and s.type == SynsetType.CONCEPT) or \
                         (pos in ['PROPN'] and s.type == SynsetType.NAMED_ENTITY \
-                                          and not s.main_sense().source.is_from_wikipedia)):
+                                          and not s.main_sense().source.is_from_wikipedia)) \
+                       and self.domains_ok(s):
                         s_ok = True
                         break
                 if s_ok:
@@ -142,13 +155,14 @@ tourism_domains = [
     'TRANSPORT_AND_TRAVEL',
 ]
     
-def test():
+def test(text='', domains=tourism_domains):
     from nlp.utils import text_to_language, make_docbin, addto_docbin
-    text = "En 1496 Caboto partió de Bristol con un buque, pero no logró ir más allá de Islandia y se vio obligado a regresar a causa de disputas con la tripulación."
+    if not text:
+        text = "En 1496 Caboto partió de Bristol con un buque, pero no logró ir más allá de Islandia y se vio obligado a regresar a causa de disputas con la tripulación."
     language, confidence = text_to_language(text)
     model = settings.LANGUAGE_MODELS[language]
     doc = model(text)
-    annotator = BabelnetAnnotator(model, domains=tourism_domains)
+    annotator = BabelnetAnnotator(model, domains=domains)
     annotator(doc)
     file_key, docbin = make_docbin('test_es', language=doc.lang_)
     file_key, docbin = addto_docbin(docbin, doc, file_key)
