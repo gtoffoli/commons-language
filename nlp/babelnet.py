68dea115d7a608ad2a94d87b27c0de599cb93d12
Giovanni Toffoli <toffoli@linkroma.it>
2023-05-15 23:36:02 +0200

    Refactored BabelNet-related modules.

--- nlp/babelnet.py
+++ nlp/babelnet.py
@@ -1,61 +1,126 @@
+from operator import itemgetter
+
 from django.conf import settings
 # from py_babelnet.calls import BabelnetAPI
 import babelnet as bn
 from babelnet import Language
 from babelnet.pos import POS
-from babelnet.domain import BabelDomain
+from babelnet.synset import SynsetType
 from babelnet.resources import BabelSynsetID
+from babelnet.data.domain import BabelDomain
+from babelnet.data.source import BabelSenseSource
+from babelnet.data.lemma import BabelLemmaType
+from babelnet.data.relation import BabelSynsetRelation
+from babelnet.data.relation import BabelPointer
+from babelnet.api import OnlineAPI
 
-"""
-def test():
-    api = BabelnetAPI(settings.BABELNET_KEY, url=settings.BABELNET_URL)
-    senses = api.get_senses(lemma = "agua", searchLang = "ES")
-    print('babelnet test', senses)
+HIGH_QUALITY = 1
 
-def getSynsetIds(lemma, searchLang='EN', pos='NOUN', targetLang=[], domains=[]):
-    api = BabelnetAPI(settings.BABELNET_KEY, url=settings.BABELNET_URL)
-    kargs = {
-        'lemma': lemma,
-        'pos': pos,
-        'searchLang': searchLang,
-    }
-    if targetLang:
-        kargs['targetLang'] = targetLang
-    if domains:
-        kargs['domains'] = domains
-    ids = api.get_synset_ids(**kargs)
-    return ids
+""" A few SynsetID representing "meta" concepts:
+    their instances are candidate terms in a domain glossary. """
+_TERM = BabelSynsetID('bn:15290634n') # (termine, terminologia) instances: prezzo 
+_JARGON = BabelSynsetID('bn:07399267n') # (termine tecnico, gergo) instances: carrucola, ..
+_SPECIALTY = BabelSynsetID('bn:14265130n') # (specialità) instances: politica monetaria
+_TERM_LIKE_CONCEPTS = [_TERM, _JARGON, _SPECIALTY,]
 
-def getSynset(id, targetLang=[]):
-    api = BabelnetAPI(settings.BABELNET_KEY, url=settings.BABELNET_URL)
-    kargs = {
-        'id': id,
-    }
-    if targetLang:
-        kargs['targetLang'] = targetLang
-    return api.get_synset(**kargs)
+# FIXME: check if correct
+SPACY_BN_POS_MAPPING = {
+        'ADJ'   : POS.ADJ, # bn.BabelPOS.ADJECTIVE,
+#       ADP   :
+        'ADV'   : POS.ADV, # bn.BabelPOS.ADVERB,
+        'AUX'   : POS.VERB, # bn.BabelPOS.VERB,
+#       CCONJ :
+#       CONJ  :
+#       DET   : POS.NOUN, # bn.BabelPOS.NOUN,
+#       EOL   :
+#       IDS   :
+#       INTJ  :
+#       NO_TAG:
+        'NOUN'  : POS.NOUN, # bn.BabelPOS.NOUN,
+        'NUM'   : POS.NOUN, # bn.BabelPOS.NOUN,
+#       PART  :
+#       PRON  : POS.NOUN, # bn.BabelPOS.NOUN,
+        'PROPN' : POS.NOUN, # bn.BabelPOS.NOUN,
+#       PUNCT :
+#       SCONJ :
+#       SPACE :
+#       SYM   :
+        'VERB'  : POS.VERB, # bn.BabelPOS.VERB,
+#       X     :
+        }
 
-def test1():
-    lemma = 'fellowship'
-    searchLang = 'EN'
-    targetLang = ['IT']
-    pos = 'NOUN'
-    domain = 'EDUCATION'
-    return getSynsetIds(lemma, pos, searchLang, targetLang, domain=domain)
+tourism_domains = [
+    'ART_ARCHITECTURE_AND_ARCHAEOLOGY',
+    'BIOLOGY',
+    'ENVIRONMENT_AND_METEOROLOGY',
+    'FOOD_DRINK_AND_TASTE',
+    'GEOGRAPHY_GEOLOGY_AND_PLACES',
+    'NAVIGATION_AND_AVIATION',
+    'TRANSPORT_AND_TRAVEL',
+]
 
-def test2():
-    id = 'bn:02844132n'
-    targetLang = ['IT']
-    return getSynset(id, targetLang)
+economy_domains = [
+    "BUSINESS_INDUSTRY_AND_FINANCE",
+    "COMMUNICATION_AND_TELECOMMUNICATION",
+    "CRAFT_ENGINEERING_AND_TECHNOLOGY",
+    "MATHEMATICS_AND_STATISTICS",
+    "POLITICS_GOVERNMENT_AND_NOBILITY",
+    "POSSESSION",
+    "TASKS_JOBS_ROUTINE_AND_EVALUATION",
+]
 
-def test3():
-    ids = ['bn:23580461n', 'bn:00034006n',]
-    targetLang = ['IT']
-    for id in ids:
-        print(id, '->', getSynset(id, targetLang))
-"""
+def isTerm(synset):
+    """ return the term-like meta-concept of which a synset is an instance, if any, or None """
+    relations = synset.outgoing_edges(ANY_HYPERNYM)
+    for relation in relations:
+        if relation.id_target in _TERM_LIKE_CONCEPTS:
+            return relation.id_target
+    return None
 
-from babelnet.api import OnlineAPI as api
+def spacy2babelnet_pos(pos):
+    return SPACY_BN_POS_MAPPING.get(pos, '')
+
+def domains_ok(synset):
+    for k, v in synset.domains.items():
+        if v >= 4.0:
+            return True
+    return False
 
 def getSynsets():
     synsets = api._get_synsets()
+
+def synsets_to_annotation(synsets):
+    """ convert synsets to annotation compatible with json storage """
+    return [[s.id.id, s.main_gloss().gloss, s.main_sense().source.source_name, \
+              sorted([[k.domain_string, v] for k,v in s.domains.items()], key=itemgetter(1), reverse=True)] \
+            for s in synsets]
+
+def query(filter={}, word='', words=[], pos='NOUN', poses=[], lang='EN', from_langs=[], domains=[], sources_in=[], sources_out=[]):
+    api = OnlineAPI()
+    if not words:
+        words = [word]
+    if not poses:
+        poses = [spacy2babelnet_pos(pos)]
+    if lang and not from_langs:
+        bn_lang = bn.language.Language.from_iso(lang)
+        from_langs = [bn_lang]
+    if not filter:
+        filter = { 'words': words, 'poses': poses, 'from_langs': from_langs}
+    synsets_1 = api._get_synsets(**filter)
+    bn_domains = set([BabelDomain[d] for d in domains])
+    synsets_2 = [ s for s in synsets_1 if not bn_domains or set(s.domains).intersection(bn_domains) ]
+    synsets = []
+    for s in synsets_2:
+        s_ok = False
+        lemma_objects = s.lemmas(bn_lang)
+        for lo in lemma_objects:
+            if lo.lemma_type == BabelLemmaType.HIGH_QUALITY and \
+               ((pos in ['VERB', 'NOUN', 'ADJ',] and s.type == SynsetType.CONCEPT) or \
+                (pos in ['PROPN'] and s.type == SynsetType.NAMED_ENTITY)) \
+               and domains_ok(s):
+                s_ok = True
+                break
+        if s_ok:
+            synsets.append(s)
+    print(synsets_to_annotation(synsets))
+    return synsets
--- nlp/babelnet_annotator.py
+++ nlp/babelnet_annotator.py
@@ -1,57 +1,11 @@
 # module derived from:
 # https://github.com/asterbini/spacy-babelnet/blob/master/spacy_babelnet/babelnet_annotator.py
 
-from operator import itemgetter
-import json
-
 from spacy.tokens.doc import Doc
 from spacy.tokens.token import Token
 from spacy.tokens import Span
 
-import babelnet as bn
-from babelnet.pos import POS
-from babelnet.data.domain import BabelDomain
-from babelnet.data.source import BabelSenseSource
-from babelnet.data.lemma import BabelLemmaType
-from babelnet.synset import SynsetType
-from babelnet.resources import BabelSynsetID
-from babelnet.api import OnlineAPI
-from _tracemalloc import start
-
-api = OnlineAPI()
-
-HIGH_QUALITY = 1
-
-from django.conf import settings
-
-# FIXME: check if correct
-SPACY_BN_POS_MAPPING = {
-        'ADJ'   : POS.ADJ, # bn.BabelPOS.ADJECTIVE,
-#       ADP   :
-        'ADV'   : POS.ADV, # bn.BabelPOS.ADVERB,
-        'AUX'   : POS.VERB, # bn.BabelPOS.VERB,
-#       CCONJ :
-#       CONJ  :
-#       DET   : POS.NOUN, # bn.BabelPOS.NOUN,
-#       EOL   :
-#       IDS   :
-#       INTJ  :
-#       NO_TAG:
-        'NOUN'  : POS.NOUN, # bn.BabelPOS.NOUN,
-        'NUM'   : POS.NOUN, # bn.BabelPOS.NOUN,
-#       PART  :
-#       PRON  : POS.NOUN, # bn.BabelPOS.NOUN,
-        'PROPN' : POS.NOUN, # bn.BabelPOS.NOUN,
-#       PUNCT :
-#       SCONJ :
-#       SPACE :
-#       SYM   :
-        'VERB'  : POS.VERB, # bn.BabelPOS.VERB,
-#       X     :
-        }
-
-def spacy2babelnet_pos(pos):
-    return SPACY_BN_POS_MAPPING.get(pos, '')
+from nlp.babelnet import *
 
 def extended_noun_chuncks(doc):
     """ try to extend noun chunks, mostly by including prepositional clauses """
@@ -79,12 +33,6 @@ def extended_noun_chuncks(doc):
         noun_chunks.append(extended)
     return noun_chunks
 
-def domains_ok(synset):
-    for k, v in synset.domains.items():
-        if v >= 4.0:
-            return True
-    return False
-
 class BabelnetAnnotator():
 
     def __init__(self, model, domains=[]):
@@ -94,12 +42,7 @@ class BabelnetAnnotator():
         self.spans = []
         self.requests = 0
         Span.set_extension('babelnet', default=None, force=True)
-
-    def synsets_to_annotation(self, synsets):
-        """ convert synsets to annotation compatible with json storage """
-        return [[s.id.id, s.main_gloss().gloss, s.main_sense().source.source_name, \
-                  sorted([[k.domain_string, v] for k,v in s.domains.items()], key=itemgetter(1), reverse=True)] \
-                for s in synsets]
+        self.api = OnlineAPI()
 
     def __call__(self, doc: Doc):
         self.doc = doc
@@ -108,7 +51,7 @@ class BabelnetAnnotator():
             spans = self.noun_chunk_annotated_spans(noun_chunk)
             if spans:
                 for span, synsets in spans:
-                    annotation = self.synsets_to_annotation(synsets)
+                    annotation = synsets_to_annotation(synsets)
                     if annotation:
                         span._.set('babelnet', annotation)
                         self.spans.append(span)
@@ -121,7 +64,7 @@ class BabelnetAnnotator():
             if token.pos_ == 'AUX' or token.lemma_ in ['essere', 'avere',]:
                 continue
             synsets = self.token_synsets(token)
-            annotation = self.synsets_to_annotation(synsets)
+            annotation = synsets_to_annotation(synsets)
             if annotation:
                 span = Span(doc, token.i, token.i+1)
                 span._.set('babelnet', annotation)
@@ -158,7 +101,7 @@ class BabelnetAnnotator():
                     filter['words'].append(span.text.replace(span[0].text, span[0].lemma_, 1))
                 if  settings.DEBUG:
                     print('filter = ', filter)
-                synsets_1 = api._get_synsets(**filter)
+                synsets_1 = self.api._get_synsets(**filter)
                 self.requests += 1
                 if  settings.DEBUG:
                     print('+', self.requests, filter['words'])
@@ -199,7 +142,7 @@ class BabelnetAnnotator():
             synsets = self.cached_synsets[key]
         else:
             filter = { 'words': words, 'poses': [spacy2babelnet_pos(pos)], 'from_langs': [self.bn_lang]}
-            synsets_1 = api._get_synsets(**filter)
+            synsets_1 = self.api._get_synsets(**filter)
             self.requests += 1
             if settings.DEBUG:
                 print('-', self.requests, filter['words'])
@@ -216,8 +159,8 @@ class BabelnetAnnotator():
                     if lo.lemma_type == BabelLemmaType.HIGH_QUALITY and \
                        ((pos in ['VERB', 'NOUN', 'ADJ',] and s.type == SynsetType.CONCEPT) or \
                         (pos in ['PROPN'] and s.type == SynsetType.NAMED_ENTITY \
-                                          and not s.main_sense().source.is_from_wikipedia)) \
-                       and domains_ok(s):
+                        #                  and not s.main_sense().source.is_from_wikipedia)) \
+                       )) and domains_ok(s):
                         s_ok = True
                         break
                 if s_ok:
@@ -225,27 +168,9 @@ class BabelnetAnnotator():
             self.cached_synsets[key] = synsets
         return synsets
 
-tourism_domains = [
-    'ART_ARCHITECTURE_AND_ARCHAEOLOGY',
-    'BIOLOGY',
-    'ENVIRONMENT_AND_METEOROLOGY',
-    'FOOD_DRINK_AND_TASTE',
-    'GEOGRAPHY_GEOLOGY_AND_PLACES',
-    'NAVIGATION_AND_AVIATION',
-    'TRANSPORT_AND_TRAVEL',
-]
+from nlp.babelnet import tourism_domains, economy_domains
 
-economy_domains = [
-    "BUSINESS_INDUSTRY_AND_FINANCE",
-    "COMMUNICATION_AND_TELECOMMUNICATION",
-    "CRAFT_ENGINEERING_AND_TECHNOLOGY",
-    "MATHEMATICS_AND_STATISTICS",
-    "POLITICS_GOVERNMENT_AND_NOBILITY",
-    "POSSESSION",
-    "TASKS_JOBS_ROUTINE_AND_EVALUATION",
-]
-    
-def test(text='', domains=tourism_domains):
+def test1(text='', domains=tourism_domains):
     from nlp.utils import text_to_language, make_docbin, addto_docbin
     if not text:
         text = "En 1496 Caboto partió de Bristol con un buque, pero no logró ir más allá de Islandia y se vio obligado a regresar a causa de disputas con la tripulación."
@@ -265,7 +190,7 @@ def test2(text='', domains=economy_domains):
     language, confidence = text_to_language(text)
     model = settings.LANGUAGE_MODELS[language]
     doc = model(text)
-    annotator = BabelnetSpanAnnotator(model, domains=domains)
+    annotator = BabelnetAnnotator(model, domains=domains)
     annotator(doc)
     return doc
 
